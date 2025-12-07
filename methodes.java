db65b7a57e13969fc5dbfca84a937a1c5470f514
moneyspread0423 <yassine.gam.2024@ihec.ucar.tn>
2025-12-08 00:10:57 +0100

    Add Gestionnaire class for patient management

--- methodes.java
+++ methodes.java
@@ -1,1 +1,23 @@
 import java.util.ArrayList;
+public class Gestionnaire {
+	
+	
+	//creer une liste pour stocker les patients
+	private ArrayList<Patient> liste_patients = new ArrayList<>();
+	// une methode qui permet d'ajouter un nouveau avec le constructeur 
+	public void ajouterPatient() {
+		this.liste_patients.add(new Patient());
+
+}
+	public void afficherList() {
+		for(int i=0;i<this.liste_patients.size();i++) {
+			System.out.println(this.liste_patients.get(i).toString());
+		}
+}
+	public Patient rechercherPatient(int cin) {
+		for(Patient p : liste_patients) {
+			if(p.getCin()==cin) return p;
+		}
+		return null;
+	}
+}
